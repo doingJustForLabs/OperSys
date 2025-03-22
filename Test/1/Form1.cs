33d48d2040bb3b365aef2929333f704bd3ada122
mamaelyaaa <poopnp586@gmail.com>
2025-03-22 16:31:31 +0300

    add Test

---
+++ Test/1/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            listView.Items.Clear();
+
+            string rootPath = pathTextBox.Text;
+
+            foreach (var dirPath in Directory.EnumerateDirectories(rootPath, "*", SearchOption.TopDirectoryOnly))
+            {
+                string folderName = new DirectoryInfo(dirPath).Name;
+                string[] values = { folderName, "Папка",  findDirSize(dirPath).ToString() + "Kb"};
+
+                listView.Items.Add(new ListViewItem(values));
+            }
+
+            foreach (var filePath in Directory.EnumerateFiles(rootPath, "*.*", SearchOption.TopDirectoryOnly))
+            {
+                string fileName = new FileInfo(filePath).Name;
+                long fileSize = new FileInfo(filePath).Length / 1024;
+
+                string[] values = { fileName, "Файл", fileSize.ToString() + " Kb"};
+
+                listView.Items.Add(new ListViewItem(values));
+
+            }
+        }
+
+        private long findDirSize(string rootDirPath)
+        {
+            long res = 0;
+
+            foreach (var dirPath in Directory.EnumerateDirectories(rootDirPath, "*", SearchOption.TopDirectoryOnly))
+            {
+                res += findDirSize(dirPath);
+            }
+
+            foreach (var filePath in Directory.EnumerateFiles(rootDirPath, "*", SearchOption.TopDirectoryOnly))
+            {
+                
+                long fileSize = new FileInfo(filePath).Length / 1024;
+                res += fileSize;
+
+            }
+
+            return res;
+        }
+    }
+}
---
+++ Test/2/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace _2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.showButton = new System.Windows.Forms.Button();
+            this.logRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.logRichTextBox);
+            this.groupBox1.Controls.Add(this.showButton);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(565, 412);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Информация о дисках";
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(178, 352);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(199, 54);
+            this.showButton.TabIndex = 1;
+            this.showButton.Text = "Show and Save";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // logRichTextBox
+            // 
+            this.logRichTextBox.Location = new System.Drawing.Point(6, 28);
+            this.logRichTextBox.Name = "logRichTextBox";
+            this.logRichTextBox.Size = new System.Drawing.Size(553, 318);
+            this.logRichTextBox.TabIndex = 2;
+            this.logRichTextBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(590, 437);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.RichTextBox logRichTextBox;
+    }
+}
+
